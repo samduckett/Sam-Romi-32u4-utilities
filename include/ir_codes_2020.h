d8a52037d4ef13b2f733c6105841eedf9d4397b3
Sam Duckett <samjduckett@gmail.com>
2026-03-17 20:21:03 -0400

    adding ir codes from 2020 remote

---
+++ include/ir_codes_2020.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define Power  69
+#define Menu   71
+
+#define Test   68
+#define Plus   64
+#define Back   67
+
+#define Reverse 7
+#define Play    21
+#define Forward 9
+
+#define NUM_0   22 
+#define Minus   25
+#define C       13
+
+#define NUM_1   12
+#define NUM_2   24
+#define NUM_3   94
+
+#define NUM_4   8
+#define NUM_5   28
+#define NUM_6   90
+
+#define NUM_7   66
+#define NUM_8   82
+#define NUM_9   74
